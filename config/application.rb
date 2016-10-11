3c4d9fbeaa865e216c446e4ef629463cac359635
Zer0 <hoatn.hedspi@gmail.com>
2016-10-11 22:49:25 +0700

    FIXED

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,14 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Btvn3
+module HoaApp
   class Application < Rails::Application
+  	config.action_view.field_error_proc = Proc.new { |html_tag, instance| 
+  "<div class=\"field_with_errors control-group error\">#{html_tag}</div>".html_safe
+}
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  root "home#home"
+  root 'static_pages#home'
+
+  # get 'static_pages/home'
+  get 'help' 	   => 'static_pages#help'
+  get 'signup'     => 'users#new'
+  post 'signup'    => 'users#create'
+  get 'login'  => 'sessions#new'
+  post 'login'  => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+  resources :users
+
+  # For details on the DSL available within this file see http://guides.rubyonrails.org/routing.html
 end
