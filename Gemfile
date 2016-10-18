c1d616cb53cf8523d9098ec6e170a1a0b0bb8e5f
Zer0 <hoatn.hedspi@gmail.com>
2016-10-18 23:54:09 +0700

    week5

--- Gemfile
+++ Gemfile
@@ -49,3 +49,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'bootstrap-sass'
 gem 'bcrypt'
+gem 'faker',          '1.6.6'
+gem 'will_paginate',           '3.1.0'
+gem 'bootstrap-will_paginate', '0.0.10'
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,48 +1,24 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @user = users(:one)
-  end
 
-  test "should get index" do
-    get users_url
-    assert_response :success
+def setup
+    @user       = users(:michael)
+    @other_user = users(:archer)
   end
-
-  test "should get new" do
-    get new_user_url
-    assert_response :success
-  end
-
-  test "should create user" do
-    assert_difference('User.count') do
-      post users_url, params: { user: { email: @user.email, name: @user.name } }
+  
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
     end
-
-    assert_redirected_to user_url(User.last)
+    assert_redirected_to login_url
   end
 
-  test "should show user" do
-    get user_url(@user)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_user_url(@user)
-    assert_response :success
-  end
-
-  test "should update user" do
-    patch user_url(@user), params: { user: { email: @user.email, name: @user.name } }
-    assert_redirected_to user_url(@user)
-  end
-
-  test "should destroy user" do
-    assert_difference('User.count', -1) do
-      delete user_url(@user)
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
     end
-
-    assert_redirected_to users_url
+    assert_redirected_to root_url
   end
 end
